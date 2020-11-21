36e7fe235cbb2b4e6cb4739895bc019bf304355b
s-weigand <s.weigand.phy@gmail.com>
2020-11-21 18:46:55 +0100

    Made gridarea assignment and unhiding of elments work automatically
    This works based on the ccs property of 'grid-template-areas' of '.container'

--- asset_src/ts/main.ts
+++ asset_src/ts/main.ts
@@ -1,11 +1,37 @@
+const onlyUnique = (value: string, index: number, self: string[]) => {
+  return self.indexOf(value) === index
+}
+
+const getUniqueAreasFromTemplateArea = (element: Element) => {
+  const ta = getComputedStyle(element).gridTemplateAreas
+  const areaArray = ta
+    .replace(/("|\.)/g, '')
+    .replace(/\s+/g, ' ')
+    .split(' ')
+  return areaArray.filter(onlyUnique)
+}
+
+const showUsedElements = () => {
+  const container = document.querySelector('.container')
+  const uniqueAreas = getUniqueAreasFromTemplateArea(container)
+  console.log(uniqueAreas)
+  for (const area of uniqueAreas) {
+    const usedArea = document.querySelector(`.${area}`)
+    if (usedArea) {
+      usedArea.classList.remove('hidden')
+    }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   let base_url = '.'
   const devPortHelperDiv = document.querySelector('div[data-dev-port]')
-  if (devPortHelperDiv !== undefined && devPortHelperDiv !== null) {
+  if (devPortHelperDiv) {
     base_url = `http://localhost:${devPortHelperDiv.getAttribute(
       'data-dev-port'
     )}`
   }
+  showUsedElements()
   const callApiRole = (role: string) => {
     console.log('clicked element with role', role)
 
