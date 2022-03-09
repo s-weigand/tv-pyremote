a0a3eb6388e93d34e2119afd095739bc42cb1fff
s-weigand <s.weigand.phy@gmail.com>
2022-03-09 14:34:58 +0100

    ⬆️ Dropped python 3.6 support

--- setup.py
+++ setup.py
@@ -27,17 +27,17 @@ test_requirements = [
 setup(
     author="Sebastian Weigand",
     author_email="s.weigand.phy@gmail.com",
-    python_requires=">=3.6.1",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="Flask app which can be used as a remote control for \
                  your PC when watching movies/series.",
